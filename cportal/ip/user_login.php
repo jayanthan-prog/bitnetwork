32441f1f9b91b31b25b457a2622966eb0106d204
jayanthan-prog <jayanthan212@gmail.com>
2024-10-23 09:28:20 +0530

    sample commit

---
+++ cportal/ip/user_login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;  charset=utf-8">
+<meta name="apple-mobile-web-app-capable" content="no"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</HEAD>
+<FRAMESET BORDER="0" FRAMESPACING="0" FRAMEBORDER="NO"> 
+  <FRAME SRC="/cportal/login.html?h=a5a4bac8&url=http://www.gstatic.com/generate_204" NAME="login_win" MARGINWIDTH="0" MARGINHEIGHT="0" scrolling="AUTO" FRAMEBORDER="NO" >
+  <NOFRAMES>
+    <script type="text/javascript" language="javascript">
+       window.location.href = "/cportal/login.html?h=a5a4bac8&url=http://www.gstatic.com/generate_204";
+    </script>
+  </NOFRAMES>
+</FRAMESET>
+</HTML>
---
+++ cportal/srp/srp_config.js
@@ -0,0 +1,35 @@
+var srp_js_rid = 203225253;
+var srp_config = {};
+
+srp_config.srp_mr_enable = 0;
+srp_config.srp_ur_enable = 0;
+srp_config.srp_vr_enable = 0;
+srp_config.srp_pr_enable = 0;
+srp_config.srp_pnp_enable = 0;
+srp_config.srp_mid_enable = 0;
+srp_config.forgot_passwd = 0;
+srp_config.srp_sng_enable = 0;
+srp_config.srp_snf_enable = 0;
+srp_config.srp_snt_enable = 0;
+srp_config.srp_sni_enable = 0;
+srp_config.srp_pmt_gw_enable = 0;
+srp_config.srp_pgw_ve_enable = 0;
+srp_config.srp_pgw_sb_enable = 0;
+
+srp_config.srp_mr_link_label = "Create Mobile Account";
+srp_config.srp_ur_link_label = "Create User Account";
+srp_config.srp_vr_link_label = "Register Prepaid Coupon";
+srp_config.srp_pr_link_label = "Prepaid Account Recharge";
+srp_config.srp_pnp_link_label = "Purchase New Plan";
+srp_config.srp_mid_link_label = "Member Login";
+srp_config.f_pass_link_label = "Forgot Password";
+srp_config.srp_pmt_gw_link_label = "Payment Gateway";
+srp_config.srp_pgw_ve_link_label = "Account Recharge";
+srp_config.srp_pgw_sb_link_label = "SmartBytes Data Topup";
+
+
+
+srp_config.status_window_type = "disable";
+
+/* DO NOT CHANGE BELOW THIS LINE */
+srp_config.cfg_site_version = 3;
